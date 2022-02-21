004012bdb442225b16699f740deee49164cb76e0
nghiamx49 <mxnghia49@gmail.com>
2022-02-21 20:00:11 +0700

    COD-74 | feat(DateTime Picker): create date time picker

---
+++ front-end/src/components/DateTimePicker.js
@@ -0,0 +1,25 @@
+import PropsType from 'prop-types';
+const DateTimePicker = ({defaultValue, onChange, maxDate, minDate, ...rest}) => {
+    return (
+        <input
+          datepicker
+          type="date"
+          className='border-1 rounded-lg w-full h-12 px-4'
+          defaultValue={defaultValue}
+          {...rest}
+          onChange={onChange}
+          min={minDate}
+          max={maxDate}
+        />
+    );
+}
+
+DateTimePicker.propsType = {
+    defaultValue: PropsType.string.isRequired,
+    onChange: PropsType.func.isRequired,
+    maxDate: PropsType.string,
+    minDate: PropsType.string
+}
+
+
+export default DateTimePicker;
