a1827512687a678f2f34f5ef6b0b7082fbc7bcd3
trinm31 <trinmgcd19833@fpt.edu.vn>
2022-03-22 16:46:45 +0700

    COD-18 | feat(timeline management): disable feature on the closure date

--- front-end/src/screens/IdeaDetail.js
+++ front-end/src/screens/IdeaDetail.js
@@ -1,33 +1,65 @@
 import { useEffect, useState, useCallback } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
-import { getSingleIdea, reactToIdea, commentToIdea, increateView } from "../apiServices";
+import {
+  getSingleIdea,
+  reactToIdea,
+  commentToIdea,
+  increateView,
+  getAllAcademic,
+  tokenRequestInterceptor,
+} from "../apiServices";
 import {
   ChevronDoubleDownIcon,
   ChevronDoubleUpIcon,
   PaperAirplaneIcon,
-  DownloadIcon
+  DownloadIcon,
 } from "@heroicons/react/outline";
+import { getNewToken } from "../store/actions/authenticateAction";
 
 import TextAria from "../components/text-area";
-import Button from '../components/button'
+import Button from "../components/button";
 import CommentItem from "../components/commentItem";
+import { date } from "yup/lib/locale";
 
 const reactionType = {
   LIKE: "Like",
-  DISLIKE: 'Dislike',
-}
+  DISLIKE: "Dislike",
+};
 
-const IdeaDetail = ({ authenticateReducer }) => {
+const IdeaDetail = ({ authenticateReducer, getNewTokenRequest }) => {
   const [ideaDetail, setIdeaDetail] = useState({});
   const [comments, setComments] = useState([]);
   const [reactions, setReactions] = useState([]);
   const [commentContent, setCommentContent] = useState("");
   const [yourReaction, setYourReaction] = useState(null);
   const { token, user } = authenticateReducer;
+  const [disableLike, setDisableLike] = useState(false);
 
   const { id } = useParams();
 
+  const getAcademicYear = useCallback(async () => {
+    const loadAcademicYear = async () => {
+      const { data, status } = await getAllAcademic(token);
+      return { data, status };
+    };
+    const { status, data } = await tokenRequestInterceptor(
+      loadAcademicYear,
+      getNewTokenRequest
+    );
+    if (status === 200) {
+      const newestYear = data[data.length - 1];
+      const closureDate = new Date(newestYear.closureDate);
+      console.log(closureDate);
+      if (closureDate < Date.now()) {
+        setDisableLike(true);
+      }
+    }
+  }, [token, getNewTokenRequest]);
+
+  useEffect(() => {
+    getAcademicYear();
+  }, [getAcademicYear]);
 
   const refreshReactionsAndCommentsList = useCallback(async () => {
     const { data, status } = await getSingleIdea(id, token);
@@ -42,8 +74,8 @@ const IdeaDetail = ({ authenticateReducer }) => {
   }, [id, token, user.id]);
 
   const inscreaseViewOfIdea = async () => {
-    await increateView(id, token)
-  }
+    await increateView(id, token);
+  };
 
   const reaction = async (reactionType) => {
     const bodyData = {
@@ -54,19 +86,19 @@ const IdeaDetail = ({ authenticateReducer }) => {
     if (status === 201) {
       refreshReactionsAndCommentsList();
     }
-  }
+  };
 
   const comment = async () => {
     const bodyData = {
       userId: user.id,
-      content: commentContent
-    }
-    const {status} = await commentToIdea(id, bodyData, token);
-    if(status === 201) {
-      setCommentContent("")
+      content: commentContent,
+    };
+    const { status } = await commentToIdea(id, bodyData, token);
+    if (status === 201) {
+      setCommentContent("");
       refreshReactionsAndCommentsList();
     }
-  }
+  };
 
   const getIdeaDetail = useCallback(async () => {
     const { data, status } = await getSingleIdea(id, token);
@@ -85,7 +117,7 @@ const IdeaDetail = ({ authenticateReducer }) => {
       });
     }
   }, [id, token, user.id]);
-  
+
   useEffect(() => {
     inscreaseViewOfIdea();
   }, []);
@@ -157,40 +189,46 @@ const IdeaDetail = ({ authenticateReducer }) => {
             Download
           </a>
         )}
-        <div className="flex gap-3 items-center mt-3">
-          <div className="flex gap-1 items-center">
-            <ChevronDoubleUpIcon
-              onClick={() => reaction(reactionType.LIKE)}
-              className={`${
-                yourReaction?.reactionType === reactionType.LIKE
-                  ? "bg-orange-200 cursor-not-allowed"
-                  : "bg-gray-200 cursor-pointer"
-              } text-gray-500 w-7 h-7  rounded-md font-medium `}
-            />
-            <span className="font-medium">
-              {reactions?.filter(
-                (item) => item.reactionType === reactionType.LIKE
-              )?.length || 0}{" "}
-              up votes
-            </span>
+        {!disableLike ? (
+          <div className="flex gap-3 items-center mt-3">
+            <div className="flex gap-1 items-center">
+              <ChevronDoubleUpIcon
+                onClick={() => reaction(reactionType.LIKE)}
+                className={`${
+                  yourReaction?.reactionType === reactionType.LIKE
+                    ? "bg-orange-200 cursor-not-allowed"
+                    : "bg-gray-200 cursor-pointer"
+                } text-gray-500 w-7 h-7  rounded-md font-medium `}
+              />
+              <span className="font-medium">
+                {reactions?.filter(
+                  (item) => item.reactionType === reactionType.LIKE
+                )?.length || 0}{" "}
+                up votes
+              </span>
+            </div>
+            <div className="flex gap-1 items-center">
+              <ChevronDoubleDownIcon
+                onClick={() => reaction(reactionType.DISLIKE)}
+                className={`${
+                  yourReaction?.reactionType === reactionType.DISLIKE
+                    ? "bg-orange-200 cursor-not-allowed"
+                    : "bg-gray-200 cursor-pointer"
+                } text-gray-500 w-7 h-7  rounded-md font-medium `}
+              />
+              <span className="font-medium">
+                {reactions?.filter(
+                  (item) => item.reactionType === reactionType.DISLIKE
+                )?.length || 0}{" "}
+                down votes
+              </span>
+            </div>
           </div>
-          <div className="flex gap-1 items-center">
-            <ChevronDoubleDownIcon
-              onClick={() => reaction(reactionType.DISLIKE)}
-              className={`${
-                yourReaction?.reactionType === reactionType.DISLIKE
-                  ? "bg-orange-200 cursor-not-allowed"
-                  : "bg-gray-200 cursor-pointer"
-              } text-gray-500 w-7 h-7  rounded-md font-medium `}
-            />
-            <span className="font-medium">
-              {reactions?.filter(
-                (item) => item.reactionType === reactionType.DISLIKE
-              )?.length || 0}{" "}
-              down votes
-            </span>
-          </div>
-        </div>
+        ) : (
+          <h2 className="text-red-800 mt-5">
+            Thumbs Up or Thumbs Down Is Close
+          </h2>
+        )}
       </div>
       <div className="container max-w-xl md:max-w-screen-lg mx-auto bg-white border shadow-sm rounded-lg mt-20">
         <div className="w-full border flex px-2 py-4 items-center gap-2">
@@ -242,4 +280,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(IdeaDetail);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getNewTokenRequest: () => dispatch(getNewToken()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(IdeaDetail);
